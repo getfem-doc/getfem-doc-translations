1df042f3599bc63b10444599cb77b4bdb2b350ba
Testuo Koyama <tkoyama010@gmail.com>
2019-01-16 02:52:15 +0900

    [skip ci] update po(t) files

---
+++ locale/update.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+# update transifex pot and local po files
+
+set -ex
+
+# required environment variables
+SPHINXINTL_TRANSIFEX_PROJECT_NAME=getfem-53-1
+# pull po files from transifex
+cd `dirname $0`
+sphinx-build -b gettext ../getfem/doc/sphinx/source pot
+sphinx-intl update -p pot -d . -l ja
+cat .tx/config
+tx push -s --skip
+rm -R ja
+tx pull -l ja
+git checkout .tx/config
+
