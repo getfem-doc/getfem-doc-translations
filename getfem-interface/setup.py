26432cea0f9fa4980f16c6541a5164c2d87239b5
Tetsuo Koyama <tkoyama010@gmail.com>
2020-08-23 23:54:47 +0900

    Update setup.py

--- getfem-interface/setup.py
+++ getfem-interface/setup.py
@@ -6,5 +6,4 @@ setup (name = 'getfem',
        version = '5.4.1',
        maintainer="Yves Renard",
        maintainer_email="Yves.Renard@insa-lyon.fr",
-       description = "This is the getfem-python-interface module",
-       ext_modules = [getfemmod])
+       description = "This is the getfem-python-interface module")
